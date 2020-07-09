1067a2922eb9bccb11c8bd5c6edf19f8867d1e08
AveYo <aveyo@yahoo.com>
2020-07-09 12:33:02 +0300

    Stop execution if STEAMPATH not found
    Windows script can have colored error message, too

--- No-Bling-builder.bat
+++ No-Bling-builder.bat
@@ -4,9 +4,15 @@
 set "STEAMPATH=C:\Program Files (x86)\Steam"
 
 :: detect STEAMPATH
-for /f "tokens=2*" %%R in ('reg query HKCU\SOFTWARE\Valve\Steam /v SteamPath') do set "steampath_reg=%%S"
+for /f "tokens=2*" %%R in ('reg query HKCU\SOFTWARE\Valve\Steam /v SteamPath 2^>nul') do set "steampath_reg=%%S"
 if not exist "%STEAMPATH%\steamapps\libraryfolders.vdf" for %%S in ("%steampath_reg%") do set "STEAMPATH=%%~fS"
-if not exist "%STEAMPATH%\steamapps\libraryfolders.vdf" echo [ERROR] STEAMPATH not found! Set it manually in the script
+
+:: Define color macros [windows10]: %<% = <ESC> and %>% = <ESC>[0m
+for /f "tokens=3 delims=." %%b in ('ver') do if %%b gtr 14393 for /f %%s in ('echo prompt $E^|cmd') do set "<=%%s"&set ">=%%s[0m"
+
+if not exist "%STEAMPATH%\steamapps\libraryfolders.vdf" (
+  echo %<%[1;7;91m [ERROR] STEAMPATH not found! Set it manually in the script %>%` & timeout -1 >nul & exit/b
+)
 
 :: prepare
 cd/d "%~dp0" & (taskkill /f /im vpkmod.exe & del /f /q vpkmod.exe) >nul 2>nul
@@ -16,7 +22,9 @@ echo @set "STEAMPATH=%STEAMPATH%" ^& call "%~dp0vpkmod.exe" -b -s ^& start "dota
 :: csc compile vpkmod tool
 for /f "tokens=* delims=" %%v in ('dir /b /s /a:-d /o:-n "%SystemRoot%\Microsoft.NET\Framework\*csc.exe"') do set "csc=%%v"
 "%csc%" /out:vpkmod.exe /target:exe /platform:anycpu /optimize /nologo /define:NOTMONO "vpkmod.cs"
-if not exist vpkmod.exe echo [ERROR] compiling VPKMOD C# code! Needs .net framework 4.0 / VS2010 compiler & pause & exit
+if not exist vpkmod.exe (
+  echo %<%[1;7;91m [ERROR] compiling VPKMOD C# code! Needs .net framework 4.0 / VS2010 compiler %>%` & timeout -1 >nul & exit
+)
 
 :: launch vpkmod with -b option for building No-Bling DOTA mod
 vpkmod.exe -b
